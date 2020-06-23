77c8acd75470aa70e555415e6a88806e8cc44e65
neb <matt@gomu.ca>
2020-06-22 21:07:48 -0400

    Add settings

---
+++ public/js/Settings.js
@@ -0,0 +1,15 @@
+// Defaults
+//Key bindings
+var JumpKey = 32;   // Space
+var FowardKey = 87; // W
+var BackKey = 83;   // S
+var leftKey = 65;   // A
+var rightKey = 68;  // D
+var crouchKey = 16; // Shift
+var reloadKey = 82; // R
+var chatKey = 84;   // T
+var exitKey = 9;    // Tab
+
+// Settings
+var warnOnClose = true;         // Warn user on tab close
+var angularSensibility = 200;   // Mouse Sensiivity
